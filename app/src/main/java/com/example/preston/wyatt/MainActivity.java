6190ddd2dd8e9f9e70a61ce3b68f19a3147cb849
preston <tunpm-16@rhodes.edu>
2015-10-13 17:53:55 -0500

    add recording and playing functionality

--- app/src/main/java/com/example/preston/wyatt/MainActivity.java
+++ app/src/main/java/com/example/preston/wyatt/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Hello Wyatt", Toast.LENGTH_SHORT).show();
+                RecordingModel.getInstance().changePlaying();
             }
         });
     }
--- app/src/main/java/com/example/preston/wyatt/RecordingModel.java
+++ app/src/main/java/com/example/preston/wyatt/RecordingModel.java
@@ -1,5 +1,12 @@
 package com.example.preston.wyatt;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.IOException;
+
 /**
  * Created by preston on 10/6/15.
  * Represents a recorder
@@ -8,10 +15,19 @@ package com.example.preston.wyatt;
  */
 public class RecordingModel {
     private boolean recording;
+    private boolean playing;
     private static RecordingModel model = null;
+    private MediaRecorder mediaRecorder = null;
+    private MediaPlayer mediaPlayer = null;
+    private String filename = null;
 
     private RecordingModel(boolean recording) {
         this.recording = recording;
+        playing = false;
+        filename =
+                Environment.getExternalStorageDirectory().getAbsolutePath()
+                + "/wyattrecording.3gp";
+
     }
 
     public static RecordingModel getInstance(){
@@ -29,6 +45,64 @@ public class RecordingModel {
     }
 
     public void changeState(){
+
         this.recording = !recording;
+        if (recording)
+            startRecording();
+        else
+            stopRecording();
     }
+
+    public void changePlaying() {
+        playing = !playing;
+
+        if (playing)
+            startPlaying();
+        else
+            stopPlaying();
+    }
+
+    private void startPlaying() {
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(filename);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        }
+        catch (IOException e) {
+            Log.e("RecordingApp", "can't load file " + filename);
+        }
+    }
+
+    private void stopPlaying() {
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    private void startRecording(){
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(filename);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+        }
+        catch (IOException e) {
+            Log.e("RecordingApp", "could not prepare");
+        }
+
+        mediaRecorder.start();
+    }
+
+    private void stopRecording() {
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+
+
+
 }
